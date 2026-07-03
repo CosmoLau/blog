95d6d76b893a29c481a15fc87924b6b83874b709
CosmoLau <cosmolau@qq.com>
2026-07-03 16:34:22 +0800

    feat: 更新追番 ID

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -182,7 +182,7 @@ export const siteConfig: SiteConfig = {
 		// Bilibili 配置
 		bilibili: {
 			// 你的 Bilibili 用户 UID
-			uid: "38932988",
+			uid: "2260014",
 		},
 		// TMDB 配置（可选，需要翻墙）
 		// tmdb: {
